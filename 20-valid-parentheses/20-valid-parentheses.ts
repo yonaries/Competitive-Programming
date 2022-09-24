f58d2970d9f9a40543b98ce84ae7ee3ba5eddeb0
Yonathan Dejene <98046049+yonaries@users.noreply.github.com>
2022-09-25 01:50:27 +0300

    Time: 76 ms (90.34%), Space: 43.9 MB (70.18%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.ts
@@ -0,0 +1,15 @@
+function isValid(s: string): boolean {
+    const opened: string[] = []
+    
+    for(let i=0;i<s.length;i++){
+        const top = opened[opened.length - 1]
+        const brc = s[i]
+        if (brc === '(' || brc === '[' || brc === '{') opened.push(brc)
+        else if (brc === ')' && top === '(') opened.pop()
+        else if (brc === ']' && top === '[') opened.pop()
+        else if (brc === '}' && top === '{') opened.pop()
+        else return false
+    }
+
+    return (opened.length === 0) ? true : false
+};
