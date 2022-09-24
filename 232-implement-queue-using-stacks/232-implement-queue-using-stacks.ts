c088cc967bd744db66badb12558edf51ce1673a3
Yonathan Dejene <98046049+yonaries@users.noreply.github.com>
2022-09-25 02:27:18 +0300

    Time: 67 ms (95.41%), Space: 43.3 MB (28.52%) - LeetHub

---
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.ts
@@ -0,0 +1,30 @@
+class MyQueue {
+    queue: number[] = []
+    constructor() {
+    }
+
+    push(x: number): void {
+        this.queue.push(x)
+    }
+
+    pop(): number {
+        return this.queue.shift()
+    }
+
+    peek(): number {
+        return this.queue[0]
+    }
+
+    empty(): boolean {
+        return this.queue.length === 0
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
