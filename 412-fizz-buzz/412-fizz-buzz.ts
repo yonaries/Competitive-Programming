46bf430ac1597e8bde592eef3387d7777610d112
Yonathan Dejene <98046049+yonaries@users.noreply.github.com>
2022-09-24 13:54:40 +0300

    Time: 133 ms (16.20%), Space: 44.6 MB (97.27%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.ts
@@ -0,0 +1,16 @@
+function fizzBuzz(n: number): string[] {
+    const arr: string[] = []
+    for(let i = 1; i <= n; i++){
+        if(i % 3 === 0 && i %5 === 0) {
+            arr.push('FizzBuzz')
+        }
+        else if(i % 3 === 0) {
+            arr.push('Fizz')
+        }
+        else if(i % 5 === 0){
+            arr.push('Buzz')
+        }
+        else arr.push(`${i}`)
+    }
+    return arr;
+};
