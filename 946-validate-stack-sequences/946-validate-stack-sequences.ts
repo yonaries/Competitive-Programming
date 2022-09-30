38f1c351a3ac6bc222ef657a05405d52b967058e
Yonathan Dejene <98046049+yonaries@users.noreply.github.com>
2022-09-30 10:10:30 +0300

    Time: 106 ms (72.73%), Space: 44.7 MB (90.91%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.ts
+++ 946-validate-stack-sequences/946-validate-stack-sequences.ts
@@ -2,13 +2,13 @@ function validateStackSequences(pushed: number[], popped: number[]): boolean {
  const stack = []
     let i = 0
 
-    for (const item of pushed) {
+   pushed.forEach( item => {
         stack.push(item)
         while (stack.length && stack[stack.length - 1] === popped[i]) {
             stack.pop()
             i++
         }
-    }
+    })
     
     return pushed.length === i
 };
