4a023562f4a810897dd2a7f59057db29faec84c4
Yonathan Dejene <98046049+yonaries@users.noreply.github.com>
2022-10-08 18:25:46 +0300

    Time: 112 ms (87.23%), Space: 45 MB (63.83%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.ts
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.ts
@@ -1,10 +1,6 @@
 function smallerNumbersThanCurrent(nums: number[]): number[] {
     let arr = new Array(nums.length).fill(0)
 
-    // for (let i = 0; i < nums.length; i++) {
-    //     arr[i] = 0;
-    // }
-
     for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j < nums.length; j++) {
             if (nums[i] > nums[j]) arr[i]++
