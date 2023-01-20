26efe1ca7311f0210f3e974be8604d86a98fbaf0
Yonathan Dejene <98046049+yonaries@users.noreply.github.com>
2023-01-20 23:34:44 +0300

    Time: 79 ms (99.33%), Space: 14.8 MB (23.34%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -6,6 +6,19 @@ class Solution:
             if list(i)!=sorted(i):
                 counter+=1
 
-        return counter 
+        return counter
+    
+#   ---------------------------
+#       Brute force Approach
+#   ---------------------------
+#         count = 0
+        
+#         for i in range(len(strs[0])):
+#             column = [ord(line[i])for line in strs]
+            
+#             if column != sorted(column):
+#                 count += 1
+        
+#         return count
             
         
