9b86341bce424acc67bf62d2b47859586e2202da
Yonatan Dejene <yonaries.dev@gmail.com>
2023-05-23 21:06:06 +0300

    Time: 1722 ms (98.49%), Space: 107.3 MB (58.00%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -0,0 +1,38 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+
+        # Create an adjacency list to represent the graph
+        adj_list = [[] for _ in range(n)]
+        
+        for u, v in edges:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+    
+        # Create a visited array to keep track of visited vertices
+        visited = [False] * n
+    
+        # Create a queue for BFS traversal
+        queue = deque([source])
+    
+        # Mark the source vertex as visited
+        visited[source] = True
+    
+        # Perform BFS traversal
+        while queue:
+            # Dequeue a vertex from the queue
+            node = queue.popleft()
+        
+            # If we have reached the destination, return True
+            if node == destination:
+                return True
+        
+            # Explore all neighbors of the current node
+            for neighbor in adj_list[node]:
+                if not visited[neighbor]:
+                    
+                    # Mark the neighbor as visited and enqueue it
+                    visited[neighbor] = True
+                    queue.append(neighbor)
+    
+        # If we have explored all possible paths and haven't found the destination, return False
+        return False
