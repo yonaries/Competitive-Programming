e49ec63916820cad14d0cc07b086806446bea3d7
Yonathan Dejene <yonaries.dev@gmail.com>
2023-10-19 23:22:27 +0300

    Time: 120 ms (11.58%), Space: 22.3 MB (13.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,28 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = {}
+        res = defaultdict(list)
         
         for string in strs:
             count = [0 for _ in range(26)]
             
             for char in string:
                 index = ord(char) - ord('a')
-                count[index] = count[index] + 1
-                
-            for i in range(len(count)):
-                count[i] = str(count[i])
-                
-            key = '-'.join(count)
-
-            if key not in res:
-                res[key] = [string]
-            else:
-                res[key] = res[key] + [string]
+                count[index] += 1
             
-        answer = []
-        for key in res:
-            answer.append(res[key])
+            res[tuple(count)].append(string)
         
-        return answer
+        return res.values()
             
                     
