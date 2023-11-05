196a040989dbb63744ea151bf67af7c339d2dfde
Yonathan Dejene <yonaries.dev@gmail.com>
2023-11-05 22:56:40 +0300

    Time: 158 ms (19.23%), Space: 18.1 MB (38.54%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort(reverse = True)
+        s.sort(reverse = True)
+        res = 0
+        
+        while s and g:
+            if g[-1] <= s[-1]: 
+                res += 1
+                g.pop()
+                s.pop()
+            else: 
+                s.pop()
+        return res
